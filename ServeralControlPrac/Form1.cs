fbc66f720ae6d5a30e6073e884c73ca5a4ff3762
wander0220 <s2018s26@e-mirim.hs.kr>
2020-07-21 09:55:21 +0900

    체크박스 소스로 구현

--- ServeralControlPrac/Form1.cs
+++ ServeralControlPrac/Form1.cs
@@ -15,6 +15,33 @@ namespace ServeralControlPrac
         public Form1()
         {
             InitializeComponent();
+
+            CheckBox checkBox1 = new CheckBox();
+            CheckBox checkBox2 = new CheckBox();
+            CheckBox checkBox3 = new CheckBox();
+            Button button = new Button();
+
+            checkBox1.Text = "감자";
+            checkBox2.Text = "고구마";
+            checkBox3.Text = "토마토";
+            button.Text = "클릭";
+
+            checkBox1.Location = new Point(10, 10);
+            checkBox2.Location = new Point(10, 40);
+            checkBox3.Location = new Point(10, 70);
+            button.Location = new Point(10, 100);
+
+            button.Click += ButtonClick;
+
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox3);
+            Controls.Add(button);
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void label1_Click(object sender, EventArgs e)
